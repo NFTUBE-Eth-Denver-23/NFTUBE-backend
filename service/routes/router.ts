dd3f870e2519a572ef251d8dd28572c9473b6bde
Ji Sang Seo <inaki96@naver.com>
2023-03-04 12:56:54 +0800

    initial settings

---
+++ service/routes/router.ts
@@ -0,0 +1,8 @@
+import { Express, Request, Response, NextFunction } from 'express';
+import colors from 'colors';
+
+const initilizeRoutes = (app: Express): void => {
+  //routings..
+};
+
+export { initilizeRoutes };
